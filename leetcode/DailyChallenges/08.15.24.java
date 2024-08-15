1d0c3f79fedbb8464dab51a08440b6f5b6fcdb38
shajahanshaik915 <154284298+shajahanshaik915@users.noreply.github.com>
2024-08-15 09:21:36 -0500

    Create 08.15.24.java

---
+++ leetcode/DailyChallenges/08.15.24.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        int five=0,ten=0;
+        for(int bill:bills){
+            if(bill==5){
+                five++;
+            }
+            else if(bill==10){
+                if(five==0) return false;
+                five--;
+                ten++;
+            }
+            else if(bill==20){
+                if((five>0&&ten>0)){
+                    five--;
+                    ten--;
+                }
+                else if(five>=3){
+                    five-=3;
+                }
+                else return false;
+            }
+            //System.out.println(m);
+        }
+        return true;
+    }
+}
